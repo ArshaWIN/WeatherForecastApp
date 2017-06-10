f2c132bd6d1ce5660517a10c694d946c95b3714c
DESKTOP-I7H23HA\Ilya <ilya.mihailenko95@mail.ru>
2017-06-11 00:59:13 +0300

    init app component

---
+++ app/src/main/java/com/mihailenko/ilya/weatherforecastapp/WeatherApplication.java
@@ -0,0 +1,8 @@
+package com.mihailenko.ilya.weatherforecastapp;
+
+/**
+ * Created by Ilya on 11.06.2017.
+ */
+
+public class WeatherApplication {
+}
---
+++ app/src/main/java/com/mihailenko/ilya/weatherforecastapp/di/app/AppModule.java
@@ -0,0 +1,8 @@
+package com.mihailenko.ilya.weatherforecastapp.di.app;
+
+/**
+ * Created by Ilya on 11.06.2017.
+ */
+
+public class AppModule {
+}
